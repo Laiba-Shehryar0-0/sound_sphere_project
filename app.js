bfa08e5fc1c0be3fd343927d5bea89dd49bcbed7
Laiba-Shehryar0-0 <137690822+Laiba-Shehryar0-0@users.noreply.github.com>
2024-12-26 01:35:11 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const indexRoutes = require("./routes/script");
+
+app.set("view engine", "ejs"); // Set EJS as the template engine
+app.set("views", path.join(__dirname, "views")); // Set views directory
+
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
+
+// Use routes
+app.use("/", indexRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ routes/script.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => res.render("home"));
+router.get("/aboutus", (req, res) => res.render("aboutus"));
+router.get("/services", (req, res) => res.render("services"));
+router.get("/portfolio", (req, res) => res.render("portfolio"));
+router.get("/contactus", (req, res) => res.render("contactus"));
+
+module.exports = router;
