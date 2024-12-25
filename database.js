bec054aa2350c9bb3ee1221abff4691d9ebc96c7
Laiba-Shehryar0-0 <137690822+Laiba-Shehryar0-0@users.noreply.github.com>
2024-12-26 03:21:29 +0500

    updated URI

--- database.js
+++ database.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // MongoDB connection string
-const MONGO_URI = 'mongodb+srv://idrees:esOb7fQL7lnWZCGi@cluster0.jxi4d.mongodb.net/playlist'; // Replace with your DB URI
+const MONGO_URI =
+  "mongodb+srv://laiba:Dj83vFpu0F18PePi@cluster0.m3qn8.mongodb.net/my_playlist"; // Replace with your DB URI
 
 // Function to connect to the database
 async function connectToDatabase() {
-    console.log("trying to connect to database");
-    try {
-        // Connect to MongoDB
-        await mongoose.connect(MONGO_URI);
-        console.log('✅✅ Successfully connected to MongoDB....');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1); // Exit the process with failure
-    }
+  console.log("trying to connect to database");
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(MONGO_URI);
+    console.log("✅✅ Successfully connected to MongoDB....");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with failure
+  }
 }
 
 module.exports = connectToDatabase;
